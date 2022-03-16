5c99bb73c4407053c0cd0e7a9c9ba7637cdda18d
Aman Sharma <amansh2786@gmail.com>
2022-03-16 22:08:11 +0530

    Updates

---
+++ precache-manifest.6298039a733cc14aad1f2ee9deb7c5fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdf9dff219c33f2c5223",
+    "url": "/resume-builder/static/css/main.ec6a3ab0.chunk.css"
+  },
+  {
+    "revision": "bdf9dff219c33f2c5223",
+    "url": "/resume-builder/static/js/main.bdf9dff2.chunk.js"
+  },
+  {
+    "revision": "6a7d9abe048c082b878d",
+    "url": "/resume-builder/static/js/1.6a7d9abe.chunk.js"
+  },
+  {
+    "revision": "e149c9d3d925a88bd2f7",
+    "url": "/resume-builder/static/js/runtime~main.e149c9d3.js"
+  },
+  {
+    "revision": "6a1502b1113d864dce6fc4c01e1d2ba7",
+    "url": "/resume-builder/index.html"
+  }
+];
